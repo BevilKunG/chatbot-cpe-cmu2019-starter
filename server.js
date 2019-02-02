b19213bd7b0f2346d2eb5704b663ea366408afc3
Parinya <parinya@Parinyas-MacBook-Pro.local>
2019-02-02 13:54:14 +0700

    in message condition

--- server.js
+++ server.js
@@ -20,18 +20,13 @@ app.get('/', function (req, res) {
 app.post('/webhook', middleware(config), (req, res) => {
   const event = req.body.events[0];
 
-  // if (event.type === 'message') {
-  //   const message = event.message;
-  //   client.replyMessage(event.replyToken, {
-  //              type: 'text',
-  //              text: message.text,
-  //            });
-  //          }
+  if (event.type === 'message') {
     const message = event.message;
     client.replyMessage(event.replyToken, {
                type: 'text',
-               text: 'This is '+message.type,
+               text: message.type,
              });
+           }
   res.send('Hel!!')
 
 })
