30d6411d72e93a5388af961cf96548420d57db92
Parinya <parinya@Parinyas-MacBook-Pro.local>
2019-02-02 10:56:43 +0700

    [Add] add webhook post route

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express')
+const middleware = require('@line/bot-sdk').middleware
 
 const app = express()
+
+const config = {
+  channelAccessToken: 'lRblqeZcwCJwTsBMvthttPvho7nNcSvRAiMBJV9glHLMZ7TgswoqXMgHDmttLtl3Mlwp4Q2ZoJyYNyFVw31muBWqUE0LdCQNX5csq8iUTHMZjdyeDR1VFcL3gjyb86qGA6+Rx/u2GufkPELL0ux/IQdB04t89/1O/w1cDnyilFU=',
+  channelSecret: 'ebf509e8a936cf361ff769f4c561dcc6'
+}
+
 
 app.get('/', function (req, res) {
     res.send('Hello World!!')
 })
+
+app.post('/webhook', middleware(config), (req, res) => {
+  console.log('webhook success')
+})
+
+
 
 app.set('port', (process.env.PORT || 4000))
 
